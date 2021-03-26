0a3604147289308abb178619b8f1c7bbab96e90e
Libor Gabrhel <gabrhel.libor@gmail.com>
2021-03-26 07:32:01 +0100

    #014: Removed body background color

--- components/global-style/global-style.tsx
+++ components/global-style/global-style.tsx
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from "styled-components"
-import { palette } from "assets/palette"
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -10,6 +9,5 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: sans-serif;
     font-weight: 400;
-    background-color: ${palette.VDM_SG};
   }
 `

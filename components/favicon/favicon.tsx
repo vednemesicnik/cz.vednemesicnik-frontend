8f83dbfbceaf38cbe1949078e0e7aacb1f8806bd
Libor Gabrhel <gabrhel.libor@gmail.com>
2021-03-26 07:30:08 +0100

    #012: Added favicon component

---
+++ components/favicon/favicon.tsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react"
+
+/**
+ * Component
+ */
+
+type Props = {
+  children?: never
+}
+
+export const Favicon = (_props: Props) => {
+  return (
+    <Fragment>
+      <link rel={"icon"} type={"image/svg+xml"} href={"/favicon.svg"} />
+      <link rel={"alternate icon"} href={"/favicon.ico"} />
+    </Fragment>
+  )
+}

1fe76a803971f77c7419fd4e14ff3b44811c6583
Libor Gabrhel <gabrhel.libor@gmail.com>
2021-04-03 14:23:25 +0200

    #011: Added colors

--- assets/palette.ts
+++ assets/palette.ts
@@ -14,4 +14,14 @@ export const palette = {
 
   WEB_BLACK: "hsl(0,0%,18%)",
   WEB_WHITE: "hsl(0,0%,98%)",
+
+  green: {
+    light: ["#5CFFA3", "#70FFAE", "#85FFBA", "#99FFC5", "#ADFFD1", "#C2FFDC", "#D6FFE8", "#EBFFF3"],
+    dark: ["#5CFFA3", "#30FF8A", "#05FF71", "#00D95E", "#00AD4B", "#008238", "#005726", "#002B13"],
+  },
+
+  blue: {
+    light: ["#0068B8", "#007FE0", "#0A95FF", "#33A7FF", "#5CB8FF", "#85CAFF", "#ADDCFF", "#D6EDFF"],
+    dark: ["#0068B8", "#005BA1", "#004E8A", "#004173", "#00345C", "#002745", "#001A2E", "#000D17"],
+  },
 }

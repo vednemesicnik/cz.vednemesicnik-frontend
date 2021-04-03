27b19712e640f4b25ec66b8818f45730989035d5
Libor Gabrhel <gabrhel.libor@gmail.com>
2021-04-03 15:08:14 +0200

    #013: Changed logo

--- assets/palette.ts
+++ assets/palette.ts
@@ -1,16 +1,6 @@
 export const palette = {
   // Spring Green
   VDM_SG: "hsl(146, 100%, 68%)",
-  VDM_SG_GRADIENT: ["hsl(146, 0%, 0%)", "hsl(146, 54%, 26%)", "hsl(146, 62%, 54%)", "hsl(146, 100%, 61%)"],
-  // Summer Yellow
-  VDM_SY: "hsl(46, 100%, 68%)",
-  VDM_SY_GRADIENT: ["hsl(46, 0%, 0%)", "hsl(46, 54%, 26%)", "hsl(46, 62%, 54%)", "hsl(46, 100%, 61%)"],
-  // Autumn Orange
-  VDM_AO: "hsl(26, 100%, 68%)",
-  VDM_AO_GRADIENT: ["hsl(26, 0%, 0%)", "hsl(26, 54%, 26%)", "hsl(26, 62%, 54%)", "hsl(26, 100%, 61%)"],
-  // Winter Blue
-  VDM_WB: "hsl(196, 100%, 68%)",
-  VDM_WB_GRADIENT: ["hsl(196, 0%, 0%)", "hsl(196, 54%, 26%)", "hsl(196, 62%, 54%)", "hsl(196, 100%, 61%)"],
 
   WEB_BLACK: "hsl(0,0%,18%)",
   WEB_WHITE: "hsl(0,0%,98%)",
@@ -24,4 +14,6 @@ export const palette = {
     light: ["#0068B8", "#007FE0", "#0A95FF", "#33A7FF", "#5CB8FF", "#85CAFF", "#ADDCFF", "#D6EDFF"],
     dark: ["#0068B8", "#005BA1", "#004E8A", "#004173", "#00345C", "#002745", "#001A2E", "#000D17"],
   },
+
+  logo: ["#5CFFA3", "#43d6a9", "#0068b8"],
 }

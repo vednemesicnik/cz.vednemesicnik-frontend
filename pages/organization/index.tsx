32243d7a502942797f4c6227c23ea0777aed058f
Libor Gabrhel <gabrhel.libor@gmail.com>
2021-05-15 10:03:23 +0200

    refactor: moved page content to markdown files

--- pages/organization/index.tsx
+++ pages/organization/index.tsx
@@ -5,10 +5,20 @@ import { PageHeader } from "components/page-header"
 import { PageFooter } from "components/page-footer"
 import { PageContent } from "components/page-content"
 import { PageHead } from "components/page-head"
-import { SupporterProfile } from "components/supporter-profile"
-import { SupportersList } from "components/supporters-list"
+import matter from "gray-matter"
+import * as fs from "fs"
+import ReactMarkdown from "react-markdown"
+import { GetStaticProps } from "next"
 
-const Organization = () => {
+type Props = {
+  content: string
+  data: {
+    editor: string
+    date: string
+  }
+}
+
+const Organization = (props: Props) => {
   return (
     <Fragment>
       <PageHead pageTitle={"Spolek"} />
@@ -19,34 +29,7 @@ const Organization = () => {
         </PageHeader>
 
         <PageContent>
-          <h1>Vedneměsíčník, z. s.</h1>
-          <p>
-            Spolek Vedneměsíčník, který vznikl v říjnu roku 2010, své cíle realizuje především publikováním časopisu
-            Vedneměsíčník, který vychází v množství nejvýše čtyř čísel za rok, každé v nákladu cca 500 výtisků. Tento
-            časopis je distribuován zdarma zejména v Literární kavárně Měsíc ve dne, na českobudějovických středních
-            školách, v klubech, čajovnách a v kině Kotva. Každé číslo je pak archivováno v Jihočeské vědecké knihovně v
-            oddělení regionálního tisku. Mezi doplňkové aktivity spolku patří organizování různých společenských a
-            kulturních akcí pro středoškolské studenty (literárních večerů a autorských čtení).
-          </p>
-          <p>Vedneměsíčník vychází za laskavé podpory:</p>
-
-          <SupportersList>
-            <SupporterProfile
-              name={"Literární kavárna Měsíc ve dne"}
-              url={"http://www.mesicvedne.cz/"}
-              logo={"/supporters/mesic-ve-dne.png"}
-            />
-            <SupporterProfile
-              name={"Biskupské gymnázium J. N. Neumanna v Č. Budějovicích"}
-              url={"http://www.bigy-cb.cz/bigy/"}
-              logo={"/supporters/bigy.png"}
-            />
-            <SupporterProfile
-              name={"Fokus České Budějovice"}
-              url={"http://www.fokus-cb.cz/"}
-              logo={"/supporters/focus.png"}
-            />
-          </SupportersList>
+          <ReactMarkdown children={props.content} />
         </PageContent>
 
         <PageFooter />
@@ -56,3 +39,15 @@ const Organization = () => {
 }
 
 export default Organization
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const file = fs.readFileSync("pages/organization/organization.md", "utf8")
+  const { data, content } = (matter(file) as unknown) as Props
+
+  return {
+    props: {
+      data,
+      content,
+    },
+  }
+}
